0e93c1d2bc482efd2b222b9432a6ae4ea6d621cf
TuMachGalan <111907470+TuMachGalan@users.noreply.github.com>
2023-01-12 17:33:01 -0300

    Finalizado

--- python-pre-entrega.py
+++ python-pre-entrega.py
@@ -1,57 +1,80 @@
-# creacion de txt para agregar los usuarios
 ruta = "C:/Users/54266/Desktop/Programacion/python/clase 11"
-f = open(ruta + "/usuarios.txt", "a")
-f.close()
-import itertools
-
-f = open(ruta + "/usuarios.txt", "r")
-users = f.read().split(":")
-users.pop()
-user_list_iter = iter(users)
-user_list_dict_object = itertools.zip_longest(user_list_iter, user_list_iter, fillvalue=None)
-user_db = dict(user_list_dict_object)
-f.close()
-
-# crear usuario
-def crearuser():
-    usuario = input("Escriba Nombre de usuario: ")
-    passw = input("Escriba su contraseña: ")
-    ruta = "C:/Users/54266/Desktop/Programacion/python/clase 11"
-    f = open(ruta + "/usuarios.txt", "a")
-    f.write(f"{usuario}:{passw}:")
-    f.close()
-
-
-def iniciar():
+def Usuarios_reg():
+    f = open(ruta + "/BaseDeDatos.txt", "r")
+    datos = list(f.read().split(":"))
+    datos.pop()
+    print(datos)
+def Login():
+    f = open(ruta + "/BaseDeDatos.txt", "r")
+    datos = list(f.read().split(":"))
+    datos.pop()
+    usuarios = []
+    passw = []
+    for clave, valor in enumerate(datos):
+        if (clave % 2 == 0):
+            usuarios.append(valor)
+    for clave, valor in enumerate(datos):
+        if (not clave % 2 == 0):
+            passw.append(valor)
+    user_db = dict(zip(usuarios,passw))
     try:
-            usuario = input("Nombre de usuario:")
-            passw = input("Contraseña:")
-            if usuario in user_db:
-                pass
-            if passw in user_db[usuario] and passw == user_db[usuario]:
-                print("Inicio Correctamente")
-            else: print("contraseña incorrecta")
-    except KeyError:
-        print("Este usuario No existe")
-        iniciar()
-
-# Menu
-print(
-    "Bienvenido \n Ingrese un numero para realizar una operacion \n 1_ Crear Usuario \n 2_ Iniciar sesión \n 3_ Ver Usuarios \n 4_ Salir")
-while True:
-    try:
-        a = int(input("Ingrese un numero: "))
-    except Exception as error:
-        print(f"ha ocurrido un error: {error}")
-    else:
-        if a < 5:
-            break
+        usuario = input("Nombre de usuario:")
+        if usuario in user_db:
+            pass
+        passw = input("Contraseña:")
+        if passw in user_db[usuario] and passw == user_db[usuario]:
+            print("Inicio Correctamente")
         else:
-            print("Ingrese un numero de lista")
-if a == 1:
-    crearuser()
-elif a == 2:
-    iniciar()
-elif a == 3:
-    print(user_db)
+            print("contraseña incorrecta")
+            Login()
+    except KeyError:
+            print("Este usuario No existe")
+            Login()
+def Checkeo(user):
+    f = open(ruta + "/BaseDeDatos.txt", "r")
+    datos = list(f.read().split(":"))
+    datos.pop()
+    f.close()
+    return user not in datos
+def Registro():
+    print("\n\n\n Bienvenido al registro\nrellene los siguientes campos:")
+    user = input("Usuario: ")
+    if Checkeo(user):
+        passw = input("Contraseña: ")
+        print(f"Recuerde su usuario es : {user}, y su contraseña es : {passw}")
+        f = open(ruta + "/BaseDeDatos.txt", "a")
+        f.write(f"{user}:{passw}:")
+        f.close()
+    else:
+        print("Este Usuario ya existe")
+def Inicio():
+    print("""
+Inicio del programa.........
+seleccione una opcion:
+1_ Inicio De sesion
+2_ Crear un nuevo usuario
+3_ Ver usuarios registrados
+4_ Finalizar programa
+        """)
+    while True:
+        try:
+            a = int(input("Ingrese un numero: "))
+        except Exception as error:
+            print(f"ha ocurrido un error: {error}")
+        else:
+            break
+    if a == 1:
+        Login()
+    elif a == 2:
+        Registro()
+    elif a == 3:
+        Usuarios_reg()
+    elif a == 4:
+        print("El Programa finalizo correctamente")
+    else:
+        print("Ingrese un numero de lista")
+        Inicio()
+
+
+Inicio()
 
